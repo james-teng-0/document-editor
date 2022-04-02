a761efbc3a406ce544ab3726048e49af0828facb
James <jamest.1402@gmail.com>
2022-04-02 22:42:51 +1100

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+// Import React dependencies.
+import React, { useState, useCallback, useMemo } from 'react'
+import { render } from 'react-dom';
+// Import the Slate editor factory.
+import {Text, createEditor, Editor, Transforms } from 'slate'
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const editor = useMemo(() => withReact(createEditor()), [])
+
+  // initial state of document
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ])
+  
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+
+  // Define a leaf rendering function that is memoized with `useCallback`.
+  const renderLeaf = useCallback(props => {
+    return <Leaf {...props} />
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Editable
+        renderElement={renderElement}
+        // Pass in the `renderLeaf` function.
+        renderLeaf={renderLeaf}
+        onKeyDown={event => {
+          if (!event.ctrlKey) {
+            return
+          }
+
+          switch (event.key) {
+            case '`': {
+              event.preventDefault()
+              const [match] = Editor.nodes(editor, {
+                match: n => n.type === 'code',
+              })
+              Transforms.setNodes(
+                editor,
+                { type: match ? null : 'code' },
+                { match: n => Editor.isBlock(editor, n) }
+              )
+              break
+            }
+
+            case 'b': {
+              event.preventDefault()
+              Transforms.setNodes(
+                editor,
+                { title: true },
+                { match: n => Text.isText(n), split: true }
+              )
+              break
+            }
+          }
+        }}
+      />
+    </Slate>
+  )
+}
+
+
+const CodeElement = props => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const DefaultElement = props => {
+  return <p {...props.attributes}>{props.children}</p>
+}
+
+const Leaf = props => {
+  return (
+    <span
+      {...props.attributes}
+      style={{ fontWeight: props.leaf.title ? 'bold' : 'normal' , fontSize: props.leaf.title ? '20px' : '15px'}}
+    >
+      {props.children}
+    </span>
+  )
 }
 
 export default App;
